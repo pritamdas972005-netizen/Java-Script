3f22e09f801becac814c539333bc189a59bf6d9c
pritamdas972005-netizen <pritamdas972005@gamil.com>
2025-10-04 15:25:08 +0530

    1 File add successful

---
+++ dowhile.js
@@ -0,0 +1,14 @@
+// Print numbers from 1 to 10.
+let i=1;
+do{
+    console.log(i);
+    i++;
+}while(i<=10);
+
+//print the multiplication table of 5.
+let a=5;
+let j=1;
+do{
+    console.log(j*a);
+    j++;
+}while(j<=10);
